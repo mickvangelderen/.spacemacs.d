d2bed77ce5bdd8d09059ddba451c058aa466f39e
Mick van Gelderen <mickvangelderen@gmail.com>
2017-03-11 13:54:21 +0100

    :sparkles: Enable version-control layer

--- init.el
+++ init.el
@@ -45,13 +45,14 @@ values."
           git-commit-fill-column 72
           git-magit-status-fullscreen t
           )
+     (version-control :variables
+                      version-control-global-margin t)
      org
      ;; (shell :variables
      ;;        shell-default-height 30
      ;;        shell-default-position 'bottom)
      ;; spell-checking
      syntax-checking
-     ;; version-control
      semantic
 
      ;; Languages
