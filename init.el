e5d04dcd1c5e0c24d0a1bfc515645595ebca5adf
Mick van Gelderen <mickvangelderen@gmail.com>
2017-04-05 11:33:38 +0200

    :sparkles: Add custom flowtype layer

--- init.el
+++ init.el
@@ -75,6 +75,7 @@ values."
      ;; Personal layers.
      emojify
      editorconfig
+     flow-type
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
