97cf139185011e1bef1aaba10f102dda3e3357a7
Mick van Gelderen <mickvangelderen@gmail.com>
2019-12-03 09:53:02 +0100

    Maximize, borderless, 10pt

--- init.el
+++ init.el
@@ -223,7 +223,7 @@ It should only modify the values of Spacemacs settings."
 
    ;; Default font or prioritized list of fonts.
    dotspacemacs-default-font '("Source Code Pro"
-                               :size 13.0
+                               :size 10.0
                                :weight normal
                                :width normal)
 
@@ -324,7 +324,7 @@ It should only modify the values of Spacemacs settings."
    ;; If non-nil the frame is maximized when Emacs starts up.
    ;; Takes effect only if `dotspacemacs-fullscreen-at-startup' is nil.
    ;; (default nil) (Emacs 24.4+ only)
-   dotspacemacs-maximized-at-startup nil
+   dotspacemacs-maximized-at-startup t
 
    ;; If non-nil the frame is undecorated when Emacs starts up. Combine this
    ;; variable with `dotspacemacs-maximized-at-startup' in OSX to obtain
