69003826b91b0097ee552eaa5775ca4e894a94e1
Mick van Gelderen <mickvangelderen@gmail.com>
2019-12-28 16:24:52 +0100

    11pt

--- init.el
+++ init.el
@@ -223,7 +223,7 @@ It should only modify the values of Spacemacs settings."
 
    ;; Default font or prioritized list of fonts.
    dotspacemacs-default-font '("Source Code Pro"
-                               :size 10.0
+                               :size 11.0
                                :weight normal
                                :width normal)
 
